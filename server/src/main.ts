fe8973cbf1dcb4a349e33a16d4ea67bbd33fe1c3
Kunals990 <kunalsable9990@gmail.com>
2025-07-09 18:19:09 +0530

    fix:allowed all origins :cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -9,16 +9,21 @@ async function bootstrap() {
   app.use(cookieParser());
 
   // ✅ CORS
+  // app.enableCors({
+  //   origin: [
+  //     'http://localhost:3000',
+  //     'http://192.168.1.4:3000',
+  //     'https://comments.kunalsable.com',
+  //     'https://comments-99.vercel.app',
+  //   ],
+  //   credentials: true,
+  //   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], 
+  //   allowedHeaders: ['Content-Type', 'Authorization'],
+  // });
+
   app.enableCors({
-    origin: [
-      'http://localhost:3000',
-      'http://192.168.1.4:3000',
-      'https://comments.kunalsable.com',
-      'https://comments-99.vercel.app',
-    ],
+    origin: true, // allows any origin
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], 
-    allowedHeaders: ['Content-Type', 'Authorization'],
   });
 
   // ✅ Global pipes
